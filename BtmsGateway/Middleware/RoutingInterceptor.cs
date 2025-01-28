00a059e68166d6bea052ba5e3ba54f2e6d1fcc5d
Ian Shimmings <ian.shimmings@hotmail.com>
2025-01-28 18:12:04 +0000

    Updated cert logging

--- BtmsGateway/Middleware/RoutingInterceptor.cs
+++ BtmsGateway/Middleware/RoutingInterceptor.cs
@@ -19,13 +19,11 @@ public class RoutingInterceptor(RequestDelegate next, IMessageRouter messageRout
                 var store = new X509Store(StoreName.Root, StoreLocation.CurrentUser);
                 store.Open(OpenFlags.ReadOnly);
                 var cert = store.Certificates.Find(X509FindType.FindByThumbprint, "5b8d8325ac276a417f6875c83533f4b8caaf338e", false).FirstOrDefault();
-                logger.Warning($"CERT ON USER: {cert?.Subject}");
+                logger.Information($"CERT: CN=HMRCRootCA2,  {cert?.Subject}");
+                cert = store.Certificates.Find(X509FindType.FindByThumbprint, "db87380eaca06ce0fc53a6562f7b0b228ec4bc5a", false).FirstOrDefault();
+                logger.Information($"CERT: CN=HMRCSICA5, {cert?.Subject}");
                 store.Close();
-                store = new X509Store(StoreName.Root, StoreLocation.LocalMachine);
-                store.Open(OpenFlags.ReadOnly);
-                cert = store.Certificates.Find(X509FindType.FindByThumbprint, "5b8d8325ac276a417f6875c83533f4b8caaf338e", false).FirstOrDefault();
-                logger.Warning($"CERT ON LOCAL: {cert?.Subject}");
-                store.Close();
+
             }
             catch (Exception ex)
             {

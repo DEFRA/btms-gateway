e2ce2bc313f1c124e39f57b00cacd0da0ff1e6be
Ian Shimmings <ian.shimmings@hotmail.com>
2025-02-06 08:31:22 +0000

    Improved logging on failure

--- BtmsGateway/Middleware/RoutingInterceptor.cs
+++ BtmsGateway/Middleware/RoutingInterceptor.cs
@@ -43,17 +43,9 @@ public class RoutingInterceptor(RequestDelegate next, IMessageRouter messageRout
         var routingResult = await messageRouter.Route(messageData);
 
         if (routingResult.RouteFound && routingResult.RouteLinkType != LinkType.None)
-        {
-            CheckResults(messageData, routingResult, "Routing");
-        }
-        else if (routingResult.RouteLinkType == LinkType.None)
-        {
-            logger.Information("{CorrelationId} Routing not configured for [{HttpString}]", messageData.CorrelationId, messageData.HttpString);
-        }
+            LogRouteFoundResults(messageData, routingResult, "Routing");
         else
-        {
-            logger.Information("{CorrelationId} Routing not supported for [{HttpString}]", messageData.CorrelationId, messageData.HttpString);
-        }
+            LogRouteNotFoundResults(messageData, routingResult, "Routing");
 
         await messageData.PopulateResponse(context.Response, routingResult);
 
@@ -65,30 +57,20 @@ public class RoutingInterceptor(RequestDelegate next, IMessageRouter messageRout
         var routingResult = await messageRouter.Fork(messageData);
 
         if (routingResult.RouteFound && routingResult.ForkLinkType != LinkType.None)
-        {
-            CheckResults(messageData, routingResult, "Forking");
-        }
-        else if (routingResult.ForkLinkType == LinkType.None)
-        {
-            logger.Information("{CorrelationId} Forking not configured for [{HttpString}]", messageData.CorrelationId, messageData.HttpString);
-        }
+            LogRouteFoundResults(messageData, routingResult, "Forking");
         else
-        {
-            logger.Information("{CorrelationId} Forking not supported for [{HttpString}]", messageData.CorrelationId, messageData.HttpString);
-        }
+            LogRouteNotFoundResults(messageData, routingResult, "Forking");
         
         metrics.RequestForked(messageData, routingResult);
     }
 
-    private void CheckResults(MessageData messageData, RoutingResult routingResult, string action)
+    private void LogRouteFoundResults(MessageData messageData, RoutingResult routingResult, string action)
     {
-        if (routingResult.RoutingSuccessful)
-        {
-            logger.Information("{CorrelationId} {Action} successful for route {RouteUrl} with response {StatusCode} {Content}", messageData.CorrelationId, action, routingResult.FullRouteLink, routingResult.StatusCode, routingResult.ResponseContent);
-        }
-        else
-        {
-            logger.Information("{CorrelationId} {Action} failed for route {RouteUrl} with status code {StatusCode}", messageData.CorrelationId, action, routingResult.FullRouteLink, routingResult.StatusCode);
-        }
+        logger.Information("{CorrelationId} {Action} {Success} for route {RouteUrl} with response {StatusCode} \"{Content}\"", messageData.CorrelationId, action, routingResult.RoutingSuccessful ? "successful" : "failed", routingResult.FullRouteLink, routingResult.StatusCode, routingResult.ResponseContent);
+    }
+
+    private void LogRouteNotFoundResults(MessageData messageData, RoutingResult routingResult, string action)
+    {
+        logger.Information("{CorrelationId} {Action} not {Reason} for [{HttpString}]", messageData.CorrelationId, action, routingResult.RouteLinkType == LinkType.None ? "configured" : "supported", messageData.HttpString);
     }
 }

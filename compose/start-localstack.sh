9a335ea49c497e82637fb2f35060011c4027453d
Tris <tristan.sellick-taylor@esynergy.co.uk>
2025-03-04 08:49:46 +0000

    test output

--- compose/start-localstack.sh
+++ compose/start-localstack.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-ENDPOINT_URL=http://sqs.eu-west-2.localhost.localstack.cloud:4966
+ENDPOINT_URL=http://localhost:4966
 
 export AWS_ENDPOINT_URL=$ENDPOINT_URL
 export AWS_REGION=eu-west-2
 export AWS_DEFAULT_REGION=eu-west-2
 export AWS_ACCESS_KEY_ID=local
-export AWS_SECRET_ACCESS_KEY=localhost
+export AWS_SECRET_ACCESS_KEY=local
 
 declare -a arr=(
         "alvs_decision_fork.fifo"
@@ -26,7 +26,10 @@ SQS_ARN=arn:aws:sqs:eu-west-2:000000000000
 
 for queueName in "${arr[@]}"
 do
-   echo "$queueName"
+   echo "QueueName: $queueName"
+   echo "Endpoint: $ENDPOINT_URL"
+   echo "SNS Param: $SNS_ARN:$queueName"
+   echo "SQS Param: $SQS_ARN:$queueName"
    aws --endpoint-url=$ENDPOINT_URL sns create-topic --attributes FifoTopic=true --name $queueName
    aws --endpoint-url=$ENDPOINT_URL sqs create-queue --attributes FifoQueue=true --queue-name $queueName
    aws --endpoint-url=$ENDPOINT_URL sns subscribe --topic-arn $SNS_ARN:$queueName --protocol sqs --notification-endpoint $SQS_ARN:$queueName

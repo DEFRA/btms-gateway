af7aced4e457c706f6d530dcac1dd12c72088752
Tris <tristan.sellick-taylor@esynergy.co.uk>
2025-03-14 18:21:35 +0000

    Re-added removed tests

---
+++ BtmsGateway.Test/EndToEnd/ErrorHandlingFromAlvsToBtmsQueueTests.cs
@@ -0,0 +1,47 @@
+using System.Net.Mime;
+using System.Text;
+using BtmsGateway.Test.TestUtils;
+using FluentAssertions;
+
+namespace BtmsGateway.Test.EndToEnd;
+
+public class ErrorHandlingFromAlvsToBtmsQueueTests() : QueueRoutingTestBase("alvs_error_fork.fifo", "alvs_error_route.fifo")
+{
+    private const string ForkPath = "/route/path/alvs-btms/error-fork-queue";
+    private const string RoutePath = "/route/path/alvs-btms/error-route-queue";
+
+    private readonly string _alvsRequestSoap = File.ReadAllText(Path.Combine(FixturesPath, "AlvsErrorHandling.xml"));
+    private readonly string _btmsRequestJson = File.ReadAllText(Path.Combine(FixturesPath, "AlvsErrorHandling.json")).LinuxLineEndings();
+
+    [Fact]
+    public async Task When_receiving_request_from_alvs_Then_should_fork_converted_json_to_btms_queue()
+    {
+        // Arrange 
+        var alvsRequestSoapContent = new StringContent(_alvsRequestSoap, Encoding.UTF8, MediaTypeNames.Application.Soap);
+
+        // Act
+        await HttpClient.PostAsync(ForkPath, alvsRequestSoapContent);
+
+        // Assert
+        var receivedMessages = await GetMessages(ForkQueueName);
+        receivedMessages.Should().NotBeEmpty();
+        receivedMessages.Should().HaveCount(1);
+        receivedMessages.FirstOrDefault().LinuxLineEndings().Should().Be(_btmsRequestJson);
+    }
+
+    [Fact]
+    public async Task When_receiving_request_from_alvs_Then_should_route_converted_json_to_btms_queue()
+    {
+        // Arrange 
+        var alvsRequestSoapContent = new StringContent(_alvsRequestSoap, Encoding.UTF8, MediaTypeNames.Application.Soap);
+
+        // Act
+        await HttpClient.PostAsync(RoutePath, alvsRequestSoapContent);
+
+        // Assert
+        var receivedMessages = await GetMessages(RouteQueueName);
+        receivedMessages.Should().NotBeEmpty();
+        receivedMessages.Should().HaveCount(1);
+        receivedMessages.FirstOrDefault().LinuxLineEndings().Should().Be(_btmsRequestJson);
+    }
+}

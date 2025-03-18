3ad96acfe4111733b629bd45e1aa37ec203e706f
Ian Shimmings <ian.shimmings@hotmail.com>
2025-03-18 16:26:53 +0000

    Changed health check to check sns

--- BtmsGateway/Services/Health/QueueHealthCheck.cs
+++ BtmsGateway/Services/Health/QueueHealthCheck.cs
@@ -1,76 +1,45 @@
 using System.Text.RegularExpressions;
-using Amazon.SQS;
-using Amazon.SQS.Model;
+using Amazon.SimpleNotificationService;
+using Amazon.SimpleNotificationService.Model;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
 using ILogger = Serilog.ILogger;
 
 namespace BtmsGateway.Services.Health;
 
-public class QueueHealthCheck : IHealthCheck
+public class QueueHealthCheck(string name, string topicArn, IAmazonSimpleNotificationService snsClient, ILogger logger)
+    : IHealthCheck
 {
-    private readonly string _queueName;
-    private readonly string _name;
-    private readonly AmazonSQSClient _sqsClient;
-    private readonly ILogger _logger;
-    private readonly string? _queueUrl;
-    private readonly Exception? _getQueueUrlException;
-
-    public QueueHealthCheck(string name, string topicArn, AmazonSQSClient sqsClient, ILogger logger)
-    {
-        _name = name;
-        _sqsClient = sqsClient;
-        _logger = logger;
-        _queueName = topicArn.Split(':')[^1];
-        try
-        {
-            _queueUrl = _sqsClient.GetQueueUrlAsync(_queueName).Result.QueueUrl;
-        }
-        catch (Exception ex)
-        {
-            _logger.Warning(ex, "HEALTH - Unable to retrieve the queue URL for {QueueName}", _queueName);
-            _getQueueUrlException = ex;
-        }
-    }
-
     public async Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = new())
     {
         using var cts = CancellationTokenSource.CreateLinkedTokenSource(cancellationToken);
         cts.CancelAfter(ConfigureHealthChecks.Timeout);
 
-        if (_queueUrl == null)
-        {
-            _logger.Warning("HEALTH - Retrieving attributes timed out for queue {QueueUrl}", _queueUrl);
-            return new HealthCheckResult(status: HealthStatus.Unhealthy, exception: _getQueueUrlException, data: new Dictionary<string, object> { { "name", _queueName } });
-        }
-
         Exception? exception = null;
-        GetQueueAttributesResponse? attributes = null;
+        GetTopicAttributesResponse? attributes = null;
         try
         {
-            attributes = await _sqsClient.GetQueueAttributesAsync(_queueUrl, ["All"], cancellationToken);
+            attributes = await snsClient.GetTopicAttributesAsync(topicArn, cancellationToken);
         }
         catch (TaskCanceledException)
         {
-            _logger.Warning("HEALTH - Retrieving attributes timed out for queue {QueueUrl}", _queueUrl);
-            exception = new TimeoutException($"The queue check cas cancelled, probably because it timed out after {ConfigureHealthChecks.Timeout.TotalSeconds} seconds");
+            logger.Warning("HEALTH - Retrieving attributes timed out for topic {Arn}", topicArn);
+            exception = new TimeoutException($"The topic check was cancelled, probably because it timed out after {ConfigureHealthChecks.Timeout.TotalSeconds} seconds");
         }
         catch (Exception ex)
         {
-            _logger.Warning(ex, "HEALTH - Retrieving attributes failed for queue {QueueUrl}", _queueUrl);
+            logger.Warning(ex, "HEALTH - Retrieving attributes failed for topic {Arn}", topicArn);
             exception = ex;
         }
 
-        var healthStatus = attributes != null ? HealthStatus.Healthy : HealthStatus.Degraded;
-
-        var data = attributes == null ? [] : new Dictionary<string, object>
+        var healthStatus = HealthStatus.Healthy;
+        var data = new Dictionary<string, object> { { "topic-arn", topicArn } };
+        if (attributes != null)
         {
-            { "queue-name", _queueName },
-            { "queue-url", _queueUrl },
-            { "approximate-number-of-messages", attributes.ApproximateNumberOfMessages },
-            { "approximate-number-of-messages-delayed", attributes.ApproximateNumberOfMessagesDelayed },
-            { "approximate-number-of-messages-not-visible", attributes.ApproximateNumberOfMessagesNotVisible },
-            { "content-length", attributes.ContentLength }
-        };
+            if ((int)attributes.HttpStatusCode < 200 || (int)attributes.HttpStatusCode > 299) healthStatus = HealthStatus.Degraded;
+
+            data.Add("content-length", attributes.ContentLength);
+            data.Add("http-status-code", attributes.HttpStatusCode);
+        }
 
         if (exception != null)
         {
@@ -80,7 +49,7 @@ public class QueueHealthCheck : IHealthCheck
 
         return new HealthCheckResult(
             status: healthStatus,
-            description: $"Queue route: {string.Join(' ', Regex.Matches(_name, "[A-Z][a-z]+", RegexOptions.None, TimeSpan.FromMilliseconds(200)))}",
+            description: $"Queue route: {string.Join(' ', Regex.Matches(name, "[A-Z][a-z]+", RegexOptions.None, TimeSpan.FromMilliseconds(200)))}",
             exception: exception,
             data: data);
     }

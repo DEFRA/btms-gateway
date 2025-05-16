58a70193de21fdb43d8a3b7787a8a9b579936309
Trinh Chuong <trinh.chuong@equalexperts.com>
2025-05-16 08:56:49 +0100

    CDMS-660: adds instance Id to the metrics dimensions

--- BtmsGateway/Services/Metrics/HealthMetrics.cs
+++ BtmsGateway/Services/Metrics/HealthMetrics.cs
@@ -1,6 +1,7 @@
 using System.Diagnostics;
 using System.Diagnostics.Metrics;
 using Amazon.CloudWatch.EMF.Model;
+using Amazon.Util;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
 
 namespace BtmsGateway.Services.Metrics;
@@ -12,6 +13,7 @@ public interface IHealthMetrics
 
 public class HealthMetrics : IHealthMetrics
 {
+    private readonly string? instanceId = EC2InstanceMetadata.InstanceId;
     private readonly Gauge<int> health;
 
     public HealthMetrics(IMeterFactory meterFactory)
@@ -27,21 +29,22 @@ public class HealthMetrics : IHealthMetrics
 
     public void ReportHealth(HealthReport report)
     {
-        health.Record((int)report.Status, BuildOverallTags());
+        health.Record((int)report.Status, BuildOverallTags(instanceId));
 
         foreach (var healthReportEntry in report.Entries)
         {
-            health.Record((int)healthReportEntry.Value.Status, BuildComponentTags(healthReportEntry));
+            health.Record((int)healthReportEntry.Value.Status, BuildComponentTags(healthReportEntry, instanceId));
         }
     }
 
-    private static TagList BuildComponentTags(KeyValuePair<string, HealthReportEntry> reportEntry)
+    private static TagList BuildComponentTags(KeyValuePair<string, HealthReportEntry> reportEntry, string? instanceId)
     {
         var tags = new TagList
         {
             { MetricsConstants.HealthTags.Service, Process.GetCurrentProcess().ProcessName },
             { MetricsConstants.HealthTags.Component, reportEntry.Key },
             { MetricsConstants.HealthTags.Description, reportEntry.Value.Description },
+            { MetricsConstants.HealthTags.InstanceId, instanceId },
         };
 
         foreach (var keyValuePair in reportEntry.Value.Data)
@@ -52,13 +55,14 @@ public class HealthMetrics : IHealthMetrics
         return tags;
     }
 
-    private static TagList BuildOverallTags()
+    private static TagList BuildOverallTags(string? instanceId)
     {
         return new TagList
         {
             { MetricsConstants.HealthTags.Service, Process.GetCurrentProcess().ProcessName },
             { MetricsConstants.HealthTags.Component, "BTMS Gateway" },
             { MetricsConstants.HealthTags.Description, "Overall health of the BTMS Gateway" },
+            { MetricsConstants.HealthTags.InstanceId, instanceId },
         };
     }
 }

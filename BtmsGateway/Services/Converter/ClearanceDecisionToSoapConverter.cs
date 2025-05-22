187004acff7c79d4c634454563cce476dda74acf
Trinh Chuong <trinh.chuong@equalexperts.com>
2025-05-22 17:10:17 +0100

    Defaults correlation id on SOAP messages created from BTMS Clearance Decisions.

--- BtmsGateway/Services/Converter/ClearanceDecisionToSoapConverter.cs
+++ BtmsGateway/Services/Converter/ClearanceDecisionToSoapConverter.cs
@@ -14,8 +14,8 @@ public static class ClearanceDecisionToSoapConverter
                 "ServiceHeader",
                 new XElement("SourceSystem", "ALVS"),
                 new XElement("DestinationSystem", "CDS"),
-                new XElement("CorrelationId", clearanceDecision.ExternalCorrelationId),
-                new XElement("ServiceCallTimestamp", clearanceDecision.Timestamp.ToString("yyyy-MM-ddTHH:mm:ss.sss"))
+                new XElement("CorrelationId", "000"),
+                new XElement("ServiceCallTimestamp", clearanceDecision.Timestamp.ToString("yyyy-MM-ddTHH:mm:ss.fff"))
             ),
             new XElement(
                 "Header",

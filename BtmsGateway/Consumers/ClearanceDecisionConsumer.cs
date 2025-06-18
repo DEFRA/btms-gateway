f25e78193592c745988ac734133a0710d6691349
Trinh Chuong <trinh.chuong@equalexperts.com>
2025-06-18 15:23:10 +0100

    CDMS-801: remove call to Data API in Clearance Decision Consumer (#128)

--- BtmsGateway/Consumers/ClearanceDecisionConsumer.cs
+++ BtmsGateway/Consumers/ClearanceDecisionConsumer.cs
@@ -3,18 +3,14 @@ using BtmsGateway.Exceptions;
 using BtmsGateway.Services.Converter;
 using BtmsGateway.Services.Routing;
 using BtmsGateway.Utils;
-using Defra.TradeImportsDataApi.Api.Client;
 using Defra.TradeImportsDataApi.Domain.CustomsDeclaration;
 using Defra.TradeImportsDataApi.Domain.Events;
 using SlimMessageBus;
 
 namespace BtmsGateway.Consumers;
 
-public class ClearanceDecisionConsumer(
-    ITradeImportsDataApiClient api,
-    IDecisionSender decisionSender,
-    ILogger<ClearanceDecisionConsumer> logger
-) : IConsumer<ResourceEvent<CustomsDeclaration>>
+public class ClearanceDecisionConsumer(IDecisionSender decisionSender, ILogger<ClearanceDecisionConsumer> logger)
+    : IConsumer<ResourceEvent<CustomsDeclaration>>
 {
     public async Task OnHandle(ResourceEvent<CustomsDeclaration> message, CancellationToken cancellationToken)
     {
@@ -33,15 +29,15 @@ public class ClearanceDecisionConsumer(
 
         try
         {
-            var customsDeclaration = await api.GetCustomsDeclaration(mrn, cancellationToken);
-
-            if (customsDeclaration is null)
+            if (message.Resource is null)
             {
-                logger.LogError("{MRN} Customs Declaration not found from Data API.", mrn);
-                throw new InvalidOperationException($"{mrn} Customs Declaration not found from Data API.");
+                logger.LogError("{MRN} Customs Declaration Resource Event contained a null resource.", mrn);
+                throw new InvalidOperationException(
+                    $"{mrn} Customs Declaration Resource Event contained a null resource."
+                );
             }
 
-            if (customsDeclaration.ClearanceDecision is null)
+            if (message.Resource.ClearanceDecision is null)
             {
                 logger.LogError("{MRN} Customs Declaration does not contain a Clearance Decision.", mrn);
                 throw new InvalidOperationException(
@@ -49,14 +45,14 @@ public class ClearanceDecisionConsumer(
                 );
             }
 
-            var soapMessage = ClearanceDecisionToSoapConverter.Convert(customsDeclaration.ClearanceDecision, mrn);
+            var soapMessage = ClearanceDecisionToSoapConverter.Convert(message.Resource.ClearanceDecision, mrn);
 
             var result = await decisionSender.SendDecisionAsync(
                 mrn,
                 soapMessage,
                 MessagingConstants.MessageSource.Btms,
                 new RoutingResult(),
-                correlationId: customsDeclaration.ClearanceDecision.CorrelationId,
+                correlationId: message.Resource.ClearanceDecision.CorrelationId,
                 cancellationToken: cancellationToken
             );
 

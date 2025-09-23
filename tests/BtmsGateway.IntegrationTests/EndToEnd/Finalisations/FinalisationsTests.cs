b72642bc191da7b695a4d74e2107ab0a5d78880d
Trinh Chuong <trinh.chuong@equalexperts.com>
2025-09-23 13:04:05 +0100

    CDMS-1116: stops forking of HMRC messages to ALVS (#169)

--- tests/BtmsGateway.IntegrationTests/EndToEnd/Finalisations/FinalisationsTests.cs
+++ tests/BtmsGateway.IntegrationTests/EndToEnd/Finalisations/FinalisationsTests.cs
@@ -4,16 +4,12 @@ using BtmsGateway.IntegrationTests.Helpers;
 using BtmsGateway.IntegrationTests.TestBase;
 using BtmsGateway.IntegrationTests.TestUtils;
 using FluentAssertions;
-using WireMock.Client;
-using WireMock.Client.Extensions;
 using Xunit.Abstractions;
 
 namespace BtmsGateway.IntegrationTests.EndToEnd.Finalisations;
 
-[Collection("UsesWireMockClient")]
-public class FinalisationsTests(WireMockClient wireMockClient, ITestOutputHelper output) : SqsTestBase(output)
+public class FinalisationsTests(ITestOutputHelper output) : SqsTestBase(output)
 {
-    private readonly IWireMockAdminApi _wireMockAdminApi = wireMockClient.WireMockAdminApi;
     private readonly string _finalisation = FixtureTest
         .UsingContent("FinalisationTemplate.xml")
         .WithRandomCorrelationId();
@@ -21,16 +17,6 @@ public class FinalisationsTests(WireMockClient wireMockClient, ITestOutputHelper
     [Fact]
     public async Task When_receiving_finalisation_from_cds_Then_should_forward_and_respond_ok()
     {
-        var postMappingBuilder = _wireMockAdminApi.GetMappingBuilder();
-        postMappingBuilder.Given(m =>
-            m.WithRequest(req =>
-                    req.UsingPost().WithPath($"/alvs{Testing.Endpoints.Finalisations.PostFinalisationNotification()}")
-                )
-                .WithResponse(rsp => rsp.WithStatusCode(HttpStatusCode.OK))
-        );
-        var postStatus = await postMappingBuilder.BuildAndPostAsync();
-        Assert.NotNull(postStatus.Guid);
-
         await PurgeQueue(InboundCustomsDeclarationProcessorQueueUrl);
 
         var httpClient = CreateHttpClient(false);
@@ -45,14 +31,6 @@ public class FinalisationsTests(WireMockClient wireMockClient, ITestOutputHelper
         verifyResponseSettings.UseTextForParameters("GatewayResponse");
         await VerifyXml(await response.Content.ReadAsStringAsync(), verifyResponseSettings);
 
-        var mockReceivedRequests = await _wireMockAdminApi.GetRequestsAsync();
-        Assert.Contains(
-            mockReceivedRequests,
-            logEntry =>
-                logEntry.Request.Path == $"/alvs{Testing.Endpoints.Finalisations.PostFinalisationNotification()}"
-                && logEntry.Request.Method == HttpMethod.Post.Method
-                && logEntry.Request.Body == _finalisation
-        );
         Assert.True(
             await AsyncWaiter.WaitForAsync(async () =>
                 (await GetQueueAttributes(InboundCustomsDeclarationProcessorQueueUrl)).ApproximateNumberOfMessages == 1

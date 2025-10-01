368851323880da1567eb022a2cee780cc1a28ad0
Tim Peel <tim.peel@equalexperts.com>
2025-10-01 15:44:59 +0100

    Remove code/files no longer being used

--- BtmsGateway/Config/ConfigureServices.cs
+++ BtmsGateway/Config/ConfigureServices.cs
@@ -9,7 +9,6 @@ using BtmsGateway.Services.Routing;
 using BtmsGateway.Utils.Http;
 using FluentValidation;
 using Microsoft.FeatureManagement;
-using ILogger = Serilog.ILogger;
 
 namespace BtmsGateway.Config;
 
@@ -21,17 +20,8 @@ public static class ConfigureServices
     public static IHttpClientBuilder? DecisionComparerHttpClientWithRetryBuilder { get; private set; }
 
     [ExcludeFromCodeCoverage]
-    public static void AddServices(this WebApplicationBuilder builder, ILogger? logger = null)
+    public static void AddServices(this WebApplicationBuilder builder)
     {
-        if (logger != null)
-        {
-            // This is added to support end to end tests that boostrap their own configuration
-            // making use of common methods such as this to add the application services. The
-            // end to end tests need to be reworked so they use an actual BTMS gateway host
-            // running in Docker.
-            builder.Services.AddSingleton(_ => logger);
-        }
-
         builder.Services.AddHttpLogging(o =>
         {
             o.RequestHeaders.Add("X-cdp-request-id");
